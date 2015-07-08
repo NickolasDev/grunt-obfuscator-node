774fffe5a35c36959c206a19bc7e2b9fa4ef98e0
Nicolaus <nickolasfrederik@gmail.com>
2015-07-07 22:06:01 -0300

    add uglify-js

--- tasks/obfuscator_node.js
+++ tasks/obfuscator_node.js
@@ -7,7 +7,7 @@
  */
 
 'use strict';
-var obfuscator = require('obfuscator');
+var obfuscator = require('../lib/obfuscator');
 var _ = require('lodash');
 
 module.exports = function(grunt) {
@@ -23,11 +23,12 @@ module.exports = function(grunt) {
     });
 
     var fn = this.async();
-    var files = this.files;
-    // Iterate over all specified file groups.
+
+
+
     this.files.forEach(function(f) {
       // Concat specified files.
-      var file = f.src.filter(function (filepath) {
+      var src = f.src.filter(function(filepath) {
         // Warn on and remove invalid source files (if nonull was set).
         if (!grunt.file.exists(filepath)) {
           grunt.log.warn('Source file "' + filepath + '" not found.');
@@ -35,32 +36,18 @@ module.exports = function(grunt) {
         } else {
           return true;
         }
-      }).map(function (filepath) {
-        return filepath;
-      });
-
-      var _options = new obfuscator.Options(file, options.path, file[0], options.strings);
-
-      // see https://github.com/mishoo/UglifyJS2/#compressor-options
-      _options.compressor = {
-        conditionals: true,
-        evaluate: true,
-        booleans: true,
-        loops: true,
-        unused: false,
-        hoist_funs: false
-      };
-
-      _options.compressor = _.merge(_options.compressor,options.compressor || {});
-
-      obfuscator(_options, function (err, data) {
-        if (err) {
-          grunt.log.error(err);
-          return fn(false);
-        }
-        grunt.file.write(f.dest,data);
-        grunt.log.writeln('File "' + f.dest + '" created.');
-        fn();
+      }).map(function(filepath) {
+        // Read file source.
+        return grunt.file.read(filepath);
+      }).join();
+      obfuscator.parse(src,options,function(err,data){
+          if (err) {
+            grunt.log.error(err);
+            return fn(false);
+          }
+          grunt.file.write(f.dest,data);
+          grunt.log.writeln('File "' + f.dest + '" created.');
+          fn();
       });
     });
   });
